edb1b3d9abeb7c9aaa6f5222c350133f34bc7e5a
haha0003 <haha0003@stud.kea.dk>
2023-11-28 15:24:02 +0100

    Initial commit

---
+++ src/Dolphin/main.java
@@ -0,0 +1,11 @@
+package Dolphin;
+
+public class main {
+    public static void main(String[] args) {
+        new main().run();
+    }
+
+    private void run() {
+        System.out.println("DinFar:)");
+    }
+}
